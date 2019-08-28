abe098bcc1203afb3bd5824834f629d98aa6bc60
sInoue <iloverinnrenn0319@gmail.com>
2019-08-28 10:30:03 +0900

    Initial commit

---
+++ src/Demotest1/App.java
@@ -0,0 +1,28 @@
+package Demotest1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton button1;
+    private JPanel panel1;
+
+    public App() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Hello world");
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
